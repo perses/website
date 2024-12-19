16d8b0b06a21270bf2bba1074de480775c2230e1
AntoineThebaud <antoine.thebaud@yahoo.fr>
2024-12-19 12:59:27 +0100

    Replace 1 provisionned dashboard by DaC
    Signed-off-by: AntoineThebaud <antoine.thebaud@yahoo.fr>

---
+++ dac/SingleGauge/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/perses/perses/go-sdk"
+	"github.com/perses/perses/go-sdk/common"
+	"github.com/perses/perses/go-sdk/dashboard"
+	"github.com/perses/perses/go-sdk/panel"
+	panelgroup "github.com/perses/perses/go-sdk/panel-group"
+	gauge "github.com/perses/perses/go-sdk/panel/gauge"
+	"github.com/perses/perses/go-sdk/prometheus/query"
+)
+
+func main() {
+	flag.Parse()
+	exec := sdk.NewExec()
+
+	// DASHBOARD
+	builder, buildErr := dashboard.New("GaugeChartPanel",
+		dashboard.ProjectName("showcase-dac"),
+
+		// PANELS
+		dashboard.AddPanelGroup("Row 1",
+			panelgroup.PanelsPerLine(3),
+			panelgroup.AddPanel("Single Gauge",
+				gauge.Chart(
+					gauge.Calculation(common.MeanCalculation),
+					gauge.Format(common.Format{
+						Unit: string(common.PercentMode),
+					}),
+					gauge.Thresholds(common.Thresholds{
+						Steps: []common.StepOption{
+							{
+								Value: 20,
+							},
+						},
+					}),
+				),
+				panel.AddQuery(
+					query.PromQL("100 - ((node_memory_MemAvailable_bytes{env=\"demo\", instance=\"demo.do.prometheus.io:9100\"} * 100) / node_memory_MemTotal_bytes{env=\"demo\", instance=\"demo.do.prometheus.io:9100\"})"),
+				),
+			),
+		),
+	)
+
+	exec.BuildDashboard(builder, buildErr)
+}
